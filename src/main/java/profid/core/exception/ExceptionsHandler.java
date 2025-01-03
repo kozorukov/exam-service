415b85514e673ac7dc0b7c418016395eb7308dcb
rkosorukov <rkosorukov@sdvor.com>
2025-01-04 02:58:45 +0300

    Добавлены обработки исключений

--- src/main/java/profid/core/exception/ExceptionsHandler.java
+++ src/main/java/profid/core/exception/ExceptionsHandler.java
@@ -1,13 +1,14 @@
 package profid.core.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ExceptionHandler {
+public class ExceptionsHandler {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handle(EntityNotFoundException e) {
         return e.getMessage();
